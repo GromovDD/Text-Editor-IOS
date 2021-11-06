3acf0c5e399178c1e88f55f5a15a3fb703f37c73
WheelaStudio <wheelastudio@gmail.com>
2021-11-06 17:23:03 +0300

    Start develop all encoding support

--- TextEditor/TextNotebook/TextDocument.swift
+++ TextEditor/TextNotebook/TextDocument.swift
@@ -5,22 +5,34 @@
 //  Created by Serega on 16.10.2021.
 //
 import SwiftUI
+import Foundation
 import UniformTypeIdentifiers
+extension Data {
+    var stringEncoding: String.Encoding? {
+        var nsString: NSString?
+        guard case let rawValue = NSString.stringEncoding(for: self, encodingOptions: nil, convertedString: &nsString, usedLossyConversion: nil), rawValue != 0 else { return nil }
+        return .init(rawValue: rawValue)
+    }
+}
 public struct TextDocument: FileDocument {
-    public let encoding : String.Encoding = .utf16
+    public let textEncoding : String.Encoding
+    public let defaultEncoding : String.Encoding = .utf16
     public static var readableContentTypes = [UTType.plainText]
     public var text : String
     init(initialText: String = "") {
         text = initialText
+        textEncoding = defaultEncoding
     }
     public func save() throws -> FileWrapper
     {
-        let data = text.data(using: encoding)!
+        let data = text.data(using: textEncoding)!
         return FileWrapper(regularFileWithContents: data)
     }
     public init(configuration: ReadConfiguration) throws {
         if let data = configuration.file.regularFileContents {
-            text = String(data: data, encoding: encoding)!
+            let soucreData = data
+            textEncoding = soucreData.stringEncoding!
+             text = String(data: soucreData, encoding: textEncoding)!
         } else {
             throw CocoaError(.fileReadCorruptFile)
         }
